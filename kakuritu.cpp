f1095db5193f029f0a4ff0af98220c5b89d15fb1
tsuchida <tsuchida.shinya413@mail.kyutech.jp>
2021-05-07 17:05:28 +0900

    sruu1

---
+++ kakuritu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#define TYOUTEN 2
+
+void create_tyoutenn(std::vector<int> &coupon)
+{
+    for (int i = 0; i < TYOUTEN; i++) {
+        coupon.push_back(0);
+    }
+}
+
+bool hanntei(std::vector<int> coupon)
+{
+    bool owaru = false;
+    
+}
+int main()
+{
+    int 
+}
