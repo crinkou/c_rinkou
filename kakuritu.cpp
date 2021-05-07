ab719c3ee54174b7df77c41eccb9a5cd4715f272
ericlab <tsuchida.shinya413@mail.kyutech.jp>
2021-05-07 22:29:13 +0900

    furu

--- kakuritu.cpp
+++ kakuritu.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
-#define TYOUTEN 2
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+static int TYOUTEN = 2;
 
 void create_tyoutenn(std::vector<int> &coupon)
 {
@@ -11,10 +14,92 @@ void create_tyoutenn(std::vector<int> &coupon)
 
 bool hanntei(std::vector<int> coupon)
 {
-    bool owaru = false;
+    for (int i = 0; i < coupon.size(); i++) {
+        if (coupon[i] == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int saikoro(std::vector<int>& coupon, int moto)
+{
+    int kazu;
+    if (coupon[moto] == 0) {
+        coupon[moto] = 1;
+    }
+    while (true) {
+        kazu = rand() % TYOUTEN;
+        if (kazu != moto) {
+            break;
+        }
+    }
+    return kazu;
+}
+
+void print_coupon(std::vector<int> coupon)
+{
+    for (int i = 0; i < coupon.size(); i++) {
+        std::cout << coupon[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+
+
+
+void one_exec(int &count_sum)
+{
+    std::vector<int> coupon;
+    create_tyoutenn(coupon);
     
+    int index = rand() % TYOUTEN;
+    int count = 0;
+    while (true) {
+        if (hanntei(coupon)) {
+            break;
+        }
+        index = saikoro(coupon, index);
+        count++;
+    }
+    count_sum += count;
+   // std::cout << "count is " << count << std::endl;
+}
+
+double coupon_try()
+{
+    int average_num = 0;
+    int try_num = 2000;
+    double result;
+    
+    for (int i = 0; i < try_num; i++) {
+        one_exec(average_num);
+    }
+    result = (double)average_num/try_num;
+    std::cout << "平均は " << result << std::endl;
+    return result;
 }
 int main()
 {
-    int 
+    srand((unsigned char)time(NULL));
+    std::ofstream ofs("/home/ericlab/OneDrive/ini.csv");
+    ofs << 0 << "," << 0 << std::endl;
+    ofs << 1 << "," << 1 << std::endl;
+    double average;
+    
+    while (TYOUTEN < 10000)
+    {
+        average = coupon_try();
+        std::cout << "現在" << TYOUTEN << "頂点" << std::endl;
+        ofs << TYOUTEN << "," << average << std::endl;
+        if (TYOUTEN < 100) {
+            TYOUTEN++;
+        }
+        else {
+            TYOUTEN+=100;
+        }
+        
+    }
+    return 0;
 }
