b559c5a9aa9a35e13f0816c1e9ebcb7f9ccb90cb
ericlab <tsuchida.shinya413@mail.kyutech.jp>
2021-05-13 22:27:24 +0900

    ffde

---
+++ Week3/question1.cpp
@@ -0,0 +1,23 @@
+#include "question1_lib.h"
+
+/*void Number_week3::set(int num){
+    num_ = num;
+}
+
+int Number_week3::get(){
+    return num_;
+}*/
+
+int main(){
+    Number_week3 number;
+    int num;
+
+    std::cout << "input integer" << std::endl;
+    std::cin >> num;
+
+    number.set(num);
+    
+    std::cout << "you type number is:" << number.get() << std::endl;
+
+    return 0;
+}
--- Week3/question1_lib.cpp
+++ Week3/question1_lib.cpp
@@ -7,17 +7,3 @@ void Number_week3::set(int num){
 int Number_week3::get(){
     return num_;
 }
-
-int main(){
-    Number_week3 number;
-    int num;
-
-    std::cout << "input integer" << std::endl;
-    std::cin >> num;
-
-    number.set(num);
-    
-    std::cout << "you type number is:" << number.get() << std::endl;
-
-    return 0;
-}
